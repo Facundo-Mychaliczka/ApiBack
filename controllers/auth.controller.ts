50bac977432637090aa5937a8ad1f208bb794a9c
Facundo-Mychaliczka <facundomychaliczka@gmail.com>
2024-02-06 00:47:07 -0300

    undo

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -91,15 +91,13 @@ export const verifyUser = async (req: Request, res: Response) => {
             return
         }
 
-        const verifiedUser = await User.findOneAndUpdate(
+        await User.findOneAndUpdate(
             {email},
             {verified: true}
         );
         
-
         res.status(200).json({
             msg: "Usuario verificado correctamente.",
-            verifiedUser
         })
         
     } catch (error) {
